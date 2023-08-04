27fa43d7d32ea13ae81d28ac889d62b28cc4758c
Gromovoi Stanislav <gromila12@rambler.ru>
2023-08-04 17:34:25 +0600

    Project 3

--- Project 3/src/redis/redis.module.ts
+++ Project 3/src/redis/redis.module.ts
@@ -1,26 +1,28 @@
-import { Module } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { createClient } from "redis";
-import { RedisService } from "./redis.service";
-import { REDIS_CLIENT, RedisClient } from "./redis-client.type";
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createClient } from 'redis';
+import { RedisService } from './redis.service';
+import { REDIS_CLIENT, RedisClient } from './redis-client.type';
 
 @Module({
   providers: [
     {
       provide: REDIS_CLIENT,
-      useFactory: async (configService: ConfigService): Promise<RedisClient> => {
+      useFactory: async (
+        configService: ConfigService
+      ): Promise<RedisClient> => {
         const client = createClient({
-          url: configService.get<string>("REDIS_URL")
+          url:
+            configService.get<string>('REDIS_URL') || 'redis://127.0.0.1:6379',
         });
-        client.on("error", err => console.log("Redis Client Error", err));
+        client.on('error', (err) => console.log('Redis Client Error', err));
         await client.connect();
         return client;
       },
-      inject: [ConfigService]
+      inject: [ConfigService],
     },
-    RedisService
+    RedisService,
   ],
-  exports: [RedisService]
+  exports: [RedisService],
 })
-export class RedisModule {
-}
+export class RedisModule {}
