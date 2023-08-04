99ee414fa112b390ede4a74ced9c1aca05d0cd71
Gromovoi Stanislav <gromila12@rambler.ru>
2023-08-04 14:01:35 +0600

    Project 3

--- Project 2/src/redis/redis.module.ts
+++ Project 2/src/redis/redis.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import  Redis from 'ioredis';
 import { RedisService } from './redis.service';
 
